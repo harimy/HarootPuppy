f3741346bae0552ce89b7606ee0e4b001cbc0c77
park-nahyun <youbetterworkbitxx@gmail.com>
2021-07-10 15:57:20 +0900

    나현 - 로그인 세션 처리(일반회원 로그인~일반회원 메인)

---
+++ HarootPuppy/src/com/haroot/mybatis/MainController.java
@@ -0,0 +1,39 @@
+package com.haroot.mybatis;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController
+{
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@RequestMapping(value="main.action", method=RequestMethod.POST)
+	public String goToMain(Model model, HttpServletRequest request, HttpServletResponse response)
+	{
+		HttpSession session = request.getSession();
+		String sid_code = (String)session.getAttribute("sid_code");
+		
+		// System.out.println(sid_code);
+		IMemberDAO dao = sqlSession.getMapper(IMemberDAO.class);
+		model.addAttribute("nickname", dao.searchNickName(sid_code));
+		
+		if(sid_code!=null)
+		{
+			return "HarootPuppyMain.jsp";
+		}
+		else
+		{
+			return "LoginForm.jsp";
+		}
+	}
+}
